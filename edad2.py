f1fb5bfadd914f5f2645c142178024a248f37a56
Udem <udem@fedora.mshome.net>
2017-06-13 14:28:54 -0600

    tarea

---
+++ edad2.py
@@ -0,0 +1,6 @@
+nombre = input("escriba su nombre: ")
+apellido = input("esccriba su apellido: ")
+edad = int(input("esccriba su edad actual: "))
+cumple= 100-edad
+cumplira = 2017 + cumple
+print( nombre, "cumpliras 100 años en el año", str(cumplira))
