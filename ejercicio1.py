f396a56e0e10f2df81b95b7ef03d70c68d1aafae
Udem <udem@fedora.mshome.net>
2017-06-20 17:17:03 -0600

    ejercicios

---
+++ ejercicio1.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+if __name__ == '__main__':
+
+	parser= argparse.ArgumentParser('valor')
+	parser.add_argument('Nombre')
+	parser.add_argument('Edad')
+	parser.add_argument('Clase')
+
+
+	args = parser.parse_args()
+
+	informacion = {
+	'Nombre': args.Nombre,
+	'Edad': args.Edad,
+	'Clase':args.Clase
+	}
+	for llave,valor in informacion.iteritems():
+		print llave,valor
