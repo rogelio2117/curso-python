5c8113d5c04dd6a9ad27f7064940c36d6f1f3ecb
Udem <udem@fedora.mshome.net>
2017-06-27 17:52:54 -0600

    pruebe

---
+++ prubadenoseque.py
@@ -0,0 +1,10 @@
+class  miclase (object)
+	a=1234
+
+	def __init__(self, cadena):
+		self.a= cadena
+	def imprimir(self):
+		print self
+		#class  MiSubClase (MiClase)
+	#def __init__(self, cadena):
+		#super(MiSubClase).__init__(cadena)
