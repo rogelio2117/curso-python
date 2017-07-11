e7b54d2c23314c8571d73d1112718bdde80013fd
Udem <udem@fedora.mshome.net>
2017-07-11 17:08:45 -0600

    variado2

--- python-GTK/variado.py
+++ python-GTK/variado.py
@@ -1,40 +1,50 @@
- gi importación
+import gi
 
-gi.require_version ( 'Gtk', '3.0')
-Gtk de importación gi.repository
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
-clase MiVentana (gtk.Window):
-def __init __ (self, args *, ** kwargs):
-super (MiVentana, auto) .__ init __ (* args, ** kwargs)
-self.set_default_size (500, 300)
-self.connect ( 'delete-evento', Gtk.main_quit)
 
-agregar_contenedor def (self):
-self.contenedor = Gtk.Grid ()
+class MiVentana(Gtk.Window):
+    def __init__(self, *args, **kwargs):
 
-Si __name__ == '__main__':
+        super(MiVentana, self).__init__(*args, **kwargs)
+        self.set_default_size(500, 300)
+        self.connect('delete-event', Gtk.main_quit)
 
-Ventana = MiVentana ()
-Ventana = gtk.Window (título = 'Variados')
-ventana.connect ( 'delete-evento', Gtk.main_quit)
-texto = Gtk.Entry ()
-texto.get_text ()
-boton = gtk.Button ( 'Boton')
-label = gtk.Label ()
-label.set_markup ( 'Texto una para contactar')
+        self.agregar_contenedor()
+        self.agregar_texto()
+        self.agregar_boton(
+        	)
+        self.agregar_label()
+        self.contenedor
 
-contenedor = Gtk.Grid ()
-contenedor.set_column_homogeneous (True)
-contenedor.set_row_homogeneous (Falso)
 
-contenedor.attach (texto, 0, 0, 3, 1)
-contenedor.attach (boton, 1, 1, 1, 1)
-contenedor.attach (etiqueta, 0, 4, 1, 1)
+    def agregar_contenedor(self):
+        self.contenedor = Gtk.Grid()
+        self.contenedor.set_column_homogeneous(True)
+        self.add(self.contenedor)
 
-ventana.add (contenedor)
-# Ventana.add (texto)
-# Ventana.add (boton)
-# Ventana.add (etiqueta)
-ventana.show_all ()
 
-Gtk.main () 
+    def agregar_texto(self):
+        self.entrada =  Gtk.Entry()
+        self.contenedor.attach(self.entrada, 0, 0, 3, 1)
+    
+
+    def agregar_boton(self):
+    	self.button = Gtk.Button('Ingresar')
+    	self.contenedor.attach(self.button, 1,1,1,1)
+
+    def agregar_label(self):
+    	self.label = Gtk.Label('Texto Ingresado')
+    	self.contenedor.attach(self.label, 0,0,3,1)
+
+
+if __name__ == '__main__':
+
+
+
+	ventana = MiVentana()
+	ventana.show_all()
+
+	Gtk.main()
+
