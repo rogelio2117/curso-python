a3543d9a9f8df614291bb9d9a8b9f1578bef1ea5
Udem <udem@fedora.mshome.net>
2017-07-12 17:25:20 -0600

    hmx

--- python-GTK/practica110717.py
+++ python-GTK/practica110717.py
@@ -22,7 +22,15 @@ class Miventana(Gtk.Window):
 
 	def agregar_entrada(self):
 		self.entrada = Gtk.Entry()
-		self.contenedor.attach(self.entrada,0,0,1,1)
+		self.entrada_monto = Gtk.Entry()
+		self.contenedor.attach(self.entrada,0,0,2,1)
+		self.contenedor.attach_next_to(
+			self.entrada_monto,
+			self.entrada,
+			Gtk.PositionType.RIGHT,
+			1,
+			1
+		)
 	
 	def agregar_boton(self):
 		self.boton = Gtk.Button('agregar')
@@ -31,14 +39,14 @@ class Miventana(Gtk.Window):
 			self.entrada,
 			Gtk.PositionType.BOTTOM,
 			1,
-			1
+			3
 		)
 		self.boton.connect('clicked', self.agregar_fila)
 
 	def agregar_lista(self):
 		
 		self.modelo = Gtk.ListStore(str, float)
-		self.modelo.append(['valor1', 1.5])
+		#self.modelo.append(['valor1', 1.5])
 
 		self.lista_activos = Gtk.TreeView(self.modelo)
 
@@ -59,15 +67,16 @@ class Miventana(Gtk.Window):
 			self.lista_activos,
 			self.boton,
 			Gtk.PositionType.BOTTOM,
-			1,
+			2,
 			1
 		)
 
-		self.modelo.append(['valor1', 1.5])
+		#self.modelo.append(['valor1', 1.5])
 
 	def agregar_fila(self, btn):
-		texo=self.entrada.get_text()
-		self.modelo.append([texto, 1.5])
+		texto=self.entrada.get_text()
+		monto= self.entrada_monto.get_text()
+		self.modelo.append([texto, float(monto)])
 
 if __name__ == '__main__':
 	ventana = Miventana()
